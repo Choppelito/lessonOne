4373bc3e6f6cf3562491b67f18b7e4d58d6285e0
HelenaOstlund <92034375+HelenaOstlund@users.noreply.github.com>
2022-01-10 10:16:46 +0100

    Squirrel

--- src/main/java/index.java
+++ src/main/java/index.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class index {
     public static void main(String[] args) {
 
@@ -8,5 +10,15 @@ public class index {
 
         storyline storyline = new storyline();
         storyline.playStory();
+
+        ArrayList<Squirrel> squirrels = new ArrayList<>();
+
+        squirrels.add(new Squirrel(5, 12, 24));
+        squirrels.add(new Squirrel(4, 7, 16));
+        squirrels.add(new Squirrel(6, 2, 12));
+        squirrels.add(new Squirrel(5, 6, 17));
+        squirrels.add(new Squirrel(4, 10, 7));
+
+        //Squirrel.eat();//
     }
 }
