d6b2ac09a3722961d89c0e82683fe338dbaff2b8
Robin Sträng <91435503+Choppelito@users.noreply.github.com>
2022-01-10 14:29:56 +0100

    almost finnished storyline

--- src/main/java/storyline.java
+++ src/main/java/storyline.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
 public class storyline {
 
     public void playStory(){
-        mainStory();
-        System.out.println("Välj 1 för grenen med antalet kottar: " + pinetree.howManyCones());
-        System.out.println("Välj 2 för grenen med antalet kottar: " + pinetree.howManyCones());
-        int choice = Integer.parseInt(System.console().readLine());
+        while(true) {
+            Scanner input = new Scanner(System.in);
+            mainStory();
+            System.out.println("Välj 1 för grenen med antalet kottar: " + pinetree.howManyCones());
+            System.out.println("Välj 2 för grenen med antalet kottar: " + pinetree.howManyCones());
+            int choice = input.nextInt();
+            if (choice == 1) {
+                System.out.println("Nu tog ugglan dig!");
+                break;
+            } else if (choice == 2) {
+                System.out.println("Du klarar dig en stund till!");
+            }
+        }
     }
 
     private void mainStory(){
